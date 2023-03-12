19b82e57b2f01cb9a9efe65188611caa141edcc6
Aldrin Sosa <sosaadrin2006@gmail.com>
2023-03-12 02:17:46 -0500

    Clean index.js code, eliminate all the act functions, delete all about edit and the localstorage management, also fix a little bug in theme.js

--- js/index.js
+++ js/index.js
@@ -1,267 +1,106 @@
-//obtain the elements from the html
+//obtains the elements from the html
 const todoInput = document.getElementById("todo-input");
 const button = document.getElementById("button-input");
 const element = document.getElementById("todo-section");
 const form = document.getElementById("form");
 
-//edit status
-var edit = false;
-
-//get the todos from the localstorage
-var todoContent = [];
-todoContent = JSON.parse(localStorage.getItem("todos")) || [];
-
-//charge the todos and listen for submits
+//charges the todos and listen for submits
 document.onload = chargeTodos();
 button.addEventListener("click", getData);
 form.addEventListener("submit", getData);
 
-//get todo from the input
+//gets the input from the textbox
 function getData(e) {
   e.preventDefault();
 
-  //get the input
+  //gets the text
   const todo = todoInput.value;
 
-  //check for the edit status
-  if (!edit) {
-    //check if had a word
-    if (todo.length == 0) {
-      return false;
-    } else {
-      //put the todo in the localstorage
-      todoContent.push(todo);
-      localStorage.setItem("todos", JSON.stringify(todoContent));
-
-      //create a todo
-      createDiv(todo);
-
-      //clean the input
-      todoInput.value = "";
-      return false;
-    }
+  //checks if the texbox is empty
+  if (todo.length == 0) {
+    return false;
   } else {
-    //return if is editing
-    return;
+    //gets an unique id
+    const id = Math.random().toString(16).slice(2);
+    //puts the todo in the localstorage
+    localStorage.setItem(id, todo); //to do
+    //creates the todo
+    createTodo(todo, id);
+    //empties the input
+    todoInput.value = "";
+    return false;
   }
 }
 
-//create the div
-function createDiv(todo) {
-  //create element and assign it
+//creates the html for the todo
+function createTodo(todo, id) {
+  //creates element and assign it
   const div = document.createElement("div");
   div.className = "todo-div";
   element.appendChild(div);
 
-  //create and assign the index
-  index = element.children.length;
-  div.id = `div${index}`;
+  //assigns the index to the div
+  div.id = `div${id}`;
 
-  //html of a div
-  div.innerHTML = `<p class="todo-text" id="p${index}">${todo}</p>
+  //html of the div
+  div.innerHTML = `<p class="todo-text" id="paragraph${id}">${todo}</p>
   <div class="buttons-div">
-  <button type="submit" class="button deleteButton" id="button${index}" ><i class="fas fa-times"></i></button>
+  <button type="submit" class="button deleteButton" id="button${id}" ><i class="fas fa-times"></i></button>
   </div>`;
 
-  //<button type="submit" class="button editButton" id="edit${index}" ><i class="fas fa-edit"></i></button>
+  /*
+  html for the edit button 
+  //<button type="submit" class="button editButton" id="edit${index}" ><i class="fas fa-edit"></i></button> 
+  */
 
-  //listen for clicks in the buttons
-  addListener(div);
-}
-
-//listen for click
-function addListener(div) {
-  //get the buttons from the div
+  //Gets the button
   const childs = div.childNodes[2].childNodes;
   const button = childs[1];
-  //const edit = childs[1];
 
-  //add event listener
-  button.addEventListener("click", clickButton);
-  //edit.addEventListener('click', clickEdit);
+  //Makes each button listen for clicks
+  button.addEventListener("click", (e) => {
+    deleteButton(e);
+  });
 }
 
 //event to delete the div
-function clickButton(e) {
-  if (edit) {
-    edit = false;
-    button.value = "Submit";
-    todoInput.value = "";
-  }
-
-  //check if is the icon
+function deleteButton(e) {
+  //checks if the the icon or the button was clicked
   if (e.target.className == "fas fa-times") {
-    //get the id
+    //gets the id of the button
     var idButton = e.target.parentNode.id;
   } else {
-    //get the id
+    //gets the id of the button
     var idButton = e.target.id;
   }
+  //cleans the id to get the index of the todo
   const index = idButton.replace("button", "");
-  const idDiv = idButton.replace("button", "div");
 
-  //remove the div and update the index of the buttons
-  rmvDiv(idDiv, index);
-  actIndexButton(index);
-  actIndexEdit(index);
+  //removes the todo
+  deleteTodo(index);
 }
 
-//event to edit the div
-function clickEdit(e) {
-  //set the edit status in true
-  edit = true;
+//removes the todo
+function deleteTodo(index) {
+  //gets the div
+  const div = document.querySelector(`#div${index}`);
 
-  //check if is the icon
-  if (e.target.className == "fas fa-edit") {
-    //get the id
-    var idButton = e.target.parentNode.id;
-  } else {
-    //get the id
-    var idButton = e.target.id;
-  }
-  const index = idButton.replace("edit", "");
-
-  //edit the div
-  editTodo(index);
-}
-
-//remove the div
-function rmvDiv(idDiv, index) {
-  //get the div
-  const div = document.querySelector(`#${idDiv}`);
-
-  //remove div
+  //removes the div
   element.removeChild(div);
 
-  //update the array of todos
-  todoContent.splice(index - 1, 1);
-
-  //update the localstorage
-  localStorage.removeItem("todos");
-  localStorage.setItem("todos", JSON.stringify(todoContent));
-
-  //update the index of the div
-  actIndexDiv(index);
+  //removes the todo in the localstorage
+  localStorage.removeItem(index);
 }
 
-//create the todos of the localstorage
+//creates the todos in the localstorage
 function chargeTodos() {
-  todoContent.forEach((existingTodo) => {
-    createDiv(existingTodo);
-  });
-}
-
-//edit the todo
-function editTodo(index) {
-  //get the p to edit
-  var p = document.getElementById(`p${index}`);
-
-  //set the value of the input and edit the button
-  todoInput.value = p.innerHTML;
-  todoInput.focus();
-  button.value = "Edit";
-
-  //listen for submits
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    //check the edit status
-    if (edit) {
-      //update the value of the todo, the edit status and the button
-      p.innerHTML = todoInput.value;
-      edit = false;
-      button.value = "Submit";
-      todoInput.value = "";
+  //checks if theres anything in the localstorage
+  if (localStorage.length !== 0) {
+    //transforms the localstorage into an object
+    localstorage = JSON.parse(JSON.stringify(localStorage));
+    //creates an todo for each one
+    for (var i in localstorage) {
+      createTodo(localstorage[i], [i]);
     }
-
-    //if is alredy editing dont do anything
-    else {
-      return;
-    }
-  });
-
-  //listen for clicks
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    //check the edit status
-    if (edit) {
-      //update the value of the todo, the edit status and the button
-      p.innerHTML = todoInput.value;
-      edit = false;
-      button.value = "Submit";
-      todoInput.value = "";
-    }
-
-    //if is alredy editing dont do anything
-    else {
-      return;
-    }
-  });
-}
-
-//update the id of the divs
-function actIndexDiv(index) {
-  //get the actual index
-  indexInt = parseInt(index);
-
-  //get all the divs
-  divs = document.querySelectorAll(".todo-div");
-
-  //iterate every div
-  divs.forEach((div) => {
-    //get the index from the id
-    indexDiv = parseInt(div.id.replace("div", ""));
-
-    //update the index if is greater than the actual index
-    if (indexDiv > indexInt) {
-      div.id = `div${indexDiv - 1}`;
-    } else {
-      return;
-    }
-  });
-}
-
-//update the id for the delete buttons
-function actIndexButton(index) {
-  //get the actual index
-  indexInt = parseInt(index);
-
-  //get all the delete buttons
-  buttons = document.querySelectorAll(".deleteButton");
-
-  //iterate every button
-  buttons.forEach((button) => {
-    //get the index from the id
-    indexButton = parseInt(button.id.replace("button", ""));
-
-    //update the index if is greater than the actual index
-    if (indexButton > indexInt) {
-      button.id = `button${indexButton - 1}`;
-    } else {
-      return;
-    }
-  });
-}
-
-//uptade the id for the edit buttons
-function actIndexEdit(index) {
-  //get the actual index
-  indexInt = parseInt(index);
-
-  //get all the edit buttons
-  edits = document.querySelectorAll(".editButton");
-
-  //iterate every button
-  edits.forEach((edit) => {
-    //get the index from the id
-    indexEdit = parseInt(edit.id.replace("edit", ""));
-
-    //update the index if is greater than the actual index
-    if (indexEdit > indexInt) {
-      edit.id = `edit${indexEdit - 1}`;
-    } else {
-      return;
-    }
-  });
+  }
 }
--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,23 @@
+// gets the button and the css variables
 let root = document.documentElement;
 let themeButton = document.getElementById("themeButton");
+
+//declares the colors
 let blue = "#14213d";
 let grey = "#e5e5e5";
 
+// listen to clicks
 themeButton.addEventListener("click", () => {
-  if (themeButton.className === "lightTheme") {
+  //checks the actual theme in the class of the button
+  if (themeButton.className === "lightTheme themeButton") {
+    //changes css variables
     root.style.setProperty("--blue", grey);
     root.style.setProperty("--grey", blue);
-    themeButton.className = "darkTheme";
+    //change tha class
+    themeButton.className = "darkTheme themeButton";
   } else {
     root.style.setProperty("--blue", blue);
     root.style.setProperty("--grey", grey);
-    themeButton.className = "lightTheme";
+    themeButton.className = "lightTheme themeButton";
   }
 });
