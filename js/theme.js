16859f4f2b3a013fe35d4f793a157bbee3541407
aldrinsosa <sosaadrin2006@gmail.com>
2023-03-13 19:47:37 -0500

    add color to the hover states of the buttons

--- js/theme.js
+++ js/theme.js
@@ -4,8 +4,10 @@ let themeButton = document.getElementById("themeButton");
 //declare the colors
 let blueLight = "#14213d";
 let greyLight = "#e5e5e5";
+let redLight = "#BF3838"
 let blueDark = "#101A2E";
 let greyDark = "#4C6088";
+let redDark = "#9B2D2D"
 
 //in case that the key doesnt exist  
 if(!localStorage.getItem("theme")) {
@@ -32,12 +34,14 @@ function setDark(){
   //changes css variables
   root.style.setProperty("--blue", blueDark);
   root.style.setProperty("--grey", greyDark);
+  root.style.setProperty("--red", redDark);
   //change tha class
   localStorage.setItem("theme","darkTheme");
 }
 
 function setLight(){
   root.style.setProperty("--blue", blueLight);
-    root.style.setProperty("--grey", greyLight);
-    localStorage.setItem("theme","lightTheme");
+  root.style.setProperty("--grey", greyLight);
+  root.style.setProperty("--red", redLight);
+  localStorage.setItem("theme","lightTheme");
 }
